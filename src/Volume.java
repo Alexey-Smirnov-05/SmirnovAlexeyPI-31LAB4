690c5e71dd8621c8ad8737ecd130dc9561586684
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:19:59 +0700

    Создание класса Volume.java

--- src/Volume.java
+++ src/Volume.java
@@ -1,2 +1,16 @@
 public class Volume {
+    private int volume;
+
+    public Volume() {
+        this.volume = 50; // начальная громкость
+    }
+
+    public void changeVolume(int newVolume) {
+        if (newVolume >= 0 && newVolume <= 100) {
+            this.volume = newVolume;
+            System.out.println("Громкость изменена на " + volume + "%");
+        } else {
+            System.out.println("Неверная громкость");
+        }
+    }
 }
