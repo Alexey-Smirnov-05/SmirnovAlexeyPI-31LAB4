dfdbb667a93bdb8c24368c6515c7c46028a52065
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:20:40 +0700

    Создание класса Playback.java

--- src/Playback.java
+++ src/Playback.java
@@ -1,2 +1,13 @@
 public class Playback {
+    private Playlist playlist;
+    private int currentTrack;
+
+    public Playback(Playlist playlist, int currentTrack) {
+        this.playlist = playlist;
+        this.currentTrack = currentTrack;
+    }
+
+    public void playSong() {
+        System.out.println("Сейчас играет: " + playlist.getTracks().get(currentTrack).getTitle());
+    }
 }
