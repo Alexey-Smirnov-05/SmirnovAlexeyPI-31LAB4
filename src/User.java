24a5f94b3442fd030722d62b2f5b0d59b59220cc
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:19:16 +0700

    Создание класса User.java

--- src/User.java
+++ src/User.java
@@ -1,2 +1,19 @@
 public class User {
+    private Playlist playlist;
+
+    public User(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public void addSongToPlaylist(Track song) {
+        playlist.addSong(song);
+    }
+
+    public void removeSongFromPlaylist(int index) {
+        playlist.removeSong(index);
+    }
+
+    public void viewPlaylist() {
+        playlist.viewSongs();
+    }
 }
