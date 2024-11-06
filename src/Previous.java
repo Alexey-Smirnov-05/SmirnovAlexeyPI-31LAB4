1561bf4c5fc95fef5e6899055f9082039ba279a3
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:33:22 +0700

    Создание класса Previous.java

--- src/Previous.java
+++ src/Previous.java
@@ -1,2 +1,13 @@
 public class Previous {
+    private Playlist playlist;
+
+    public Previous(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public void previousSong() {
+        playlist.setCurrentTrack((playlist.getCurrentTrack() - 1 + playlist.getTotalNumberOfTracks()) % playlist.getTotalNumberOfTracks());
+        Playback playback = new Playback(playlist, playlist.getCurrentTrack());
+        playback.playSong();
+    }
 }
