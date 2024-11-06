1d617190760035d6f291ef28bfbc20f5b368be70
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:16:25 +0700

    Создание класса Track.java

---
+++ src/Previous.java
@@ -0,0 +1,2 @@
+public class Previous {
+}
---
+++ src/Track.java
@@ -0,0 +1,17 @@
+public class Track {
+    private String title;
+
+    public Track() {}
+
+    public Track(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
