9b945747cb2e06c9b9702c677f8c5928bafe917c
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:21:14 +0700

    Создание класса Pause.java

--- src/Pause.java
+++ src/Pause.java
@@ -1,2 +1,11 @@
 public class Pause {
+    private Playlist playlist;
+
+    public Pause(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public void pauseSong() {
+        System.out.println("На паузе");
+    }
 }
