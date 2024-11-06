c6c21421680226438d013582db5a6a7c38270216
Alexey <alexsmirnovalex05@gmail.com>
2024-11-06 21:31:48 +0700

    Создание класса Skip.java

--- src/Skip.java
+++ src/Skip.java
@@ -1,2 +1,13 @@
 public class Skip {
+    private Playlist playlist;
+
+    public Skip(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public void skipSong() {
+        playlist.setCurrentTrack((playlist.getCurrentTrack() + 1) % playlist.getTotalNumberOfTracks());
+        Playback playback = new Playback(playlist, playlist.getCurrentTrack());
+        playback.playSong();
+    }
 }
